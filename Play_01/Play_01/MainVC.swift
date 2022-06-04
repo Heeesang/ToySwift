4c21edcac49cd79ba43eed6e1e221efa874eead9
Heesang <80827042+Heeesang@users.noreply.github.com>
2022-06-04 16:12:48 +0900

    Play_01 create project

---
+++ Play_01/Play_01/MainVC.swift
@@ -0,0 +1,13 @@
+//
+//  MainVC.swift
+//  Play_01
+//
+//  Created by 곽희상 on 2022/06/04.
+//
+
+import Foundation
+import UIKit
+
+class MainVC: UIViewController{
+    
+}
---
+++ Play_01/Play_01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Play_01
+//
+//  Created by 곽희상 on 2022/06/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
