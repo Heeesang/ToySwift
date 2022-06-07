2dc0b4fdeace14e564c31b976ccd6cb6505c230a
Heesang <80827042+Heeesang@users.noreply.github.com>
2022-06-07 23:30:18 +0900

    MapView

--- Play_01/Play_01/MainVC.swift
+++ Play_01/Play_01/MainVC.swift
@@ -5,9 +5,22 @@
 //  Created by 곽희상 on 2022/06/04.
 //
 
-import Foundation
 import UIKit
+import MapKit
 
-class MainVC: UIViewController{
+class MainVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    let myMap = MapView()
+    
+    override func loadView() {
+        view = myMap
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        myMap.map.delegate = self
+        myMap.map.setRegion(MKCoordinateRegion(center: 0, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)), animated: true)
+    }
     
 }
