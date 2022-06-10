42e87b25e5c883c7f824f85751734dad8a6a87ed
Heesang <80827042+Heeesang@users.noreply.github.com>
2022-06-10 11:53:42 +0900

    play_02 set

---
+++ Play_02/Play_02/MainVC.swift
@@ -0,0 +1,16 @@
+//
+//  MainVC.swift
+//  Play_02
+//
+//  Created by 곽희상 on 2022/06/10.
+//
+
+import Foundation
+import UIKit
+
+class MainVC: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
