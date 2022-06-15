d1cf75258d75de07fbbdc9d8fb5dd48cbbcc0096
Heesang <80827042+Heeesang@users.noreply.github.com>
2022-06-15 23:45:17 +0900

    collection View

--- Play_03/Play_03/MainVC.swift
+++ Play_03/Play_03/MainVC.swift
@@ -8,6 +8,29 @@
 import Foundation
 import UIKit
 
-class MainVC: UIViewController{
+class MainVC: UIViewController, UISearchBarDelegate{
     
+    let collectionView: UICollectionView = {
+        let flowlayout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: flowlayout)
+        
+        return cv
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(collectionView)
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        collectionView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = false
+        collectionView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+
+        collectionView.backgroundColor = .yellow
+        
+    }
 }
