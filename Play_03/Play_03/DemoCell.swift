3e786870871df663594a304d5d0b03d6bc6f65ff
Heesang <80827042+Heeesang@users.noreply.github.com>
2022-06-16 23:26:59 +0900

    collectionView

---
+++ Play_03/Play_03/DemoCell.swift
@@ -0,0 +1,12 @@
+//
+//  DemoCellCollectionViewCell.swift
+//  Play_03
+//
+//  Created by 곽희상 on 2022/06/16.
+//
+
+import UIKit
+
+class DemoCell: UICollectionViewCell {
+    
+}
--- Play_03/Play_03/MainVC.swift
+++ Play_03/Play_03/MainVC.swift
@@ -8,6 +8,8 @@
 import Foundation
 import UIKit
 
+let cellID = "Cell"
+
 class MainVC: UIViewController, UISearchBarDelegate{
     
     let collectionView: UICollectionView = {
@@ -27,10 +29,28 @@ class MainVC: UIViewController, UISearchBarDelegate{
         
         collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
         collectionView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = false
+        collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
         collectionView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
 
         collectionView.backgroundColor = .yellow
         
+        collectionView.register(DemoCell.self, forCellWithReuseIdentifier: "Cell")
     }
+}
+
+extension MainVC: UICollectionViewDataSource {
+   
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+    }
+    
+}
+
+extension MainVC: UICollectionViewDelegate{
+    
 }
