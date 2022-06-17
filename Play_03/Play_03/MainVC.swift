fef9b8288fdaca40afb666cdaff0957e42cb5f23
Heesang <80827042+Heeesang@users.noreply.github.com>
2022-06-17 13:52:42 +0900

    collectionView

--- Play_03/Play_03/MainVC.swift
+++ Play_03/Play_03/MainVC.swift
@@ -23,6 +23,9 @@ class MainVC: UIViewController, UISearchBarDelegate{
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
         view.addSubview(collectionView)
         
         collectionView.translatesAutoresizingMaskIntoConstraints = false
@@ -34,23 +37,42 @@ class MainVC: UIViewController, UISearchBarDelegate{
 
         collectionView.backgroundColor = .yellow
         
-        collectionView.register(DemoCell.self, forCellWithReuseIdentifier: "Cell")
+        collectionView.register(DemoCell.self, forCellWithReuseIdentifier: cellID)
     }
 }
 
-extension MainVC: UICollectionViewDataSource {
-   
+extension MainVC: UICollectionViewDataSource, UICollectionViewDelegate {
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        
+        return 4
     }
     
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath) as! DemoCell
         
+        cell.backgroundColor = .red
+        
+        return cell
     }
     
 }
 
-extension MainVC: UICollectionViewDelegate{
+extension MainVC: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: (view.frame.width - 30 - 10) / 4 , height: (view.frame.width - 30 - 10) / 4)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+    }
+    
+    
     
 }
+
